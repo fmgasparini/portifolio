89532d9b16fe1ec9cb2cb133b3d16cf530d92307
fernando <fernandom.gasparini@gmail.com>
2020-06-16 08:51:03 -0300

    auth

--- 2.auth/app.js
+++ 2.auth/app.js
@@ -8,10 +8,16 @@ var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
 var multer = require('multer');
 var upload = multer({dest: '/uploads'});
-var flash = require('connect-flash');
+var flash = require('express-flash');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
+
+// Resolve deprecated objects
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
 
 var indexRouter = require('./routes/index');
 var authRouter = require('./routes/auth');
@@ -34,6 +40,15 @@ app.use(session({
 	saveUninitialized: true,
 	resave: true
 }));
+
+// Flash messages
+app.use(flash());
+
+app.use(function(req, res, next){
+    res.locals.success_messages = req.flash('success_messages');
+    res.locals.error_messages = req.flash('error_messages');
+    next();
+});
 
 // Passport
 app.use(passport.initialize());
--- 2.auth/routes/auth.js
+++ 2.auth/routes/auth.js
@@ -4,6 +4,8 @@ var multer = require('multer');
 var upload = multer({dest: 'uploads/'});
 var { check, validationResult } = require('express-validator');
 
+var User = require('../models/user');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -42,26 +44,44 @@ router.post('/register', upload.single('profileimage'),
     var errors = validationResult(req);
 
     if (errors.isEmpty()){
-      	null;
+
+    	var name = req.body.name;
+	  	var email = req.body.email;
+  		var username = req.body.username;
+  		var password = req.body.password;
+  		var confirmPassword = req.body.confirmpassword;
+
+    	if(req.file){
+ 		 	var profileimage = req.file.filename;
+    	} else {
+       		var profileimage = 'noimage.jpg'
+   		 };
+      	
+      	var newUser = new User({
+  			name: name,
+  			email: email,
+  			username: username,
+  			password: password,
+  			profileimage: profileimage
+  		});
+
+  		User.createUser(newUser, function(err, user){
+  			if(err) throw err;
+  			console.log(user);
+  		});
+
+  		req.flash('success', 'You are now registered and can login');
+
+  		res.location('/');
+  		res.redirect('/');
+
   	} else {
   		res.render('register', {
       		errors: errors.array(),
       		title: 'Register'
       	});
-  	}
-
-  	var name = req.body.name;
-  	var email = req.body.email;
-  	var username = req.body.username;
-  	var password = req.body.password;
-  	var confirmPassword = req.body.confirmpassword;
-
-	if(req.file){
- 	 	var profileimage = req.file.filename;
-    } else {
-  		var profileimage = 'nomeimage.jpg';
-    };
-
+  	};
+  	
 });
 
 module.exports = router;
